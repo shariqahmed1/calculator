1120606b269808779e85b61836bc4966a72855b4
Shariq Ahmed <shariq.ahmed525@gmail.com>
2018-03-10 12:30:57 -0800

    yeah

---
+++ cal.js
@@ -0,0 +1,24 @@
+function add()
+{
+    document.cal.display.value += '+';
+}
+
+function sub()
+{
+    document.cal.display.value += '-';
+}
+
+function mul()
+{
+    document.cal.display.value += '*';
+}
+
+function dvd()
+{
+    document.cal.display.value += '/';
+}
+
+function mod()
+{
+    document.cal.display.value += '%';
+}
